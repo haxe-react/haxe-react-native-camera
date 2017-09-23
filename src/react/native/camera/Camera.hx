7f761600f1e077aeab344e4af7296fcf08eaf13b
Kevin Leung <kevinresol@gmail.com>
2017-09-23 16:58:59 +0800

    Some more props

--- src/react/native/camera/Camera.hx
+++ src/react/native/camera/Camera.hx
@@ -6,9 +6,50 @@ import react.native.component.props.*;
 @:jsRequire('react-native-camera', 'default')
 extern class Camera extends ReactComponentOfProps<{
 	> ViewProps,
+	?aspect:Aspect,
+	?captureMode:CaptureMode,
+	?captureTarget:CaptureTarget,
+	?captureQuality:CaptureQuality,
 	?onBarCodeRead:{type:String, data:String}->Void,
 	?barCodeTypes:Array<String>
-}> {}
+	
+}> {
+	static var constants:{
+		Aspect:{
+			fit:Aspect,
+			fill:Aspect,
+			stretch:Aspect,
+		},
+		CaptureMode:{
+			still:CaptureMode,
+			video:CaptureMode,
+		},
+		CaptureTarget:{
+			cameraRoll:CaptureTarget,
+			disk:CaptureTarget,
+			temp:CaptureTarget,
+		},
+		CaptureQuality: {
+			high:CaptureQuality,
+			medium:CaptureQuality,
+			low:CaptureQuality,
+			photo:CaptureQuality,
+		}
+	}
+	
+	function capture(?options:{}):js.Promise<{
+		path:String,
+		width:Int,
+		height:Int,
+		duration:Int,
+		size:Int,
+	}>;
+}
+
+extern abstract Aspect(Dynamic) {}
+extern abstract CaptureTarget(Dynamic) {}
+extern abstract CaptureMode(Dynamic) {}
+extern abstract CaptureQuality(Dynamic) {}
 
 @:jsRequire('react-native-camera', 'default.constants.BarCodeType')
 @:enum extern abstract BarCodeType(String) to String {
